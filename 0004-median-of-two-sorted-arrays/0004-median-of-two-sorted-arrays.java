77735113f1b8de3bd3a8c963f831db411fe07ee8
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-06-26 14:22:55 -0700

    Time: 2 ms (100.00%), Space: 44.8 MB (29.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,53 +1,38 @@
 class Solution {
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-
-        if (m > n) {
+        if(nums1.length > nums2.length) 
             return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int total = m + n;
-        int half = (total + 1) / 2;
-
-        int left = 0;
-        int right = m;
-
-        var result = 0.0;
-
-        while (left <= right) {
-            int i = (right + left) / 2;
-            int j = half - i;
-            //System.out.println("total:"+ total+ " i=" + i +" j=" + j);
-
-            // get the four points around possible median
-            int left1 = (i > 0) ? nums1[i - 1] : Integer.MIN_VALUE;
-            int right1 = (i < m) ? nums1[i] : Integer.MAX_VALUE;
-            int left2 = (j > 0) ? nums2[j - 1] : Integer.MIN_VALUE;
-            int right2 = (j < n) ? nums2[j] : Integer.MAX_VALUE;
-
-            // partition is correct
-            if (left1 <= right2 && left2 <= right1) {
-                // even
-                if (total % 2 == 0) {
-                    result =
-                        (Math.max(left1, left2) + Math.min(right1, right2)) /
-                        2.0;
-                    // odd
-                } else {
-                    result = Math.max(left1, left2);
-                }
-                break;
+        
+        int x = nums1.length;
+        int y = nums2.length;
+        
+        int low = 0;
+        int high = x;
+        
+        while(low <= high){
+            
+            int partitionX = (low + high)/2;
+            int partitionY = (x + y + 1)/2 - partitionX ; 
+            
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+            
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if((x + y) % 2 == 0 )
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                else
+                    return ((double)Math.max(maxLeftX, maxLeftY));
             }
-            // partition is wrong (update left/right pointers)
-            else if (left1 > right2) {
-                right = i - 1;
-            } else {
-                left = i + 1;
-            }
+            else if(maxLeftX > minRightY)
+                high = partitionX - 1;
+            else 
+                low = partitionX + 1;
         }
-
-        return result;
+        
+        
+        throw new IllegalArgumentException();
     }
 }
