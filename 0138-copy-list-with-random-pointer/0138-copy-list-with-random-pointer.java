6adadb10d3120c194f3bb365f2233a204209b635
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-06-29 17:04:12 -0700

    Time: 0 ms (100.00%), Space: 43.5 MB (26.66%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,6 +15,9 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        /*
+        // TC = O(N); SC = O(N)
+        
         HashMap<Node, Node> mapToCopy = new HashMap<>();
         Node p1 = head;
         
@@ -34,5 +37,43 @@ class Solution {
         }
         
         return mapToCopy.get(head);
+        */
+        
+        // TC = O(N); SC = O(1)
+        if(head == null) 
+            return null;
+        Node p1 = head;
+        while(p1 != null){
+            Node p1_next = p1.next;
+            Node copy_node = new Node(p1.val);
+            p1.next = copy_node;
+            copy_node.next = p1_next;
+            p1 = p1_next;
+        }
+        // Connect random nodes
+        p1 = head;
+        
+        while(p1 != null){
+            if(p1.random != null){
+                p1.next.random = p1.random.next;
+            }
+            p1 = p1.next.next;
+        }
+        
+        // Restore next pointers
+        p1 = head;
+        Node copy_head = head.next;
+        
+        while(p1 != null){
+            Node copy_node = p1.next;
+            Node next_node = p1.next.next;
+            p1.next = next_node;
+            
+            if(next_node != null)
+                copy_node.next = next_node.next;
+            p1 = next_node;
+        }
+        
+        return copy_head;
     }
 }
