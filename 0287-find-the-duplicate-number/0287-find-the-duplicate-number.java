0104b2affd0ea0793e2f918a2a75f9284e29cbfc
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-06-30 11:46:21 -0700

    Time: 4 ms (87.50%), Space: 56.5 MB (71.24%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        /*
+        // Solution 1: TC = O(N), SC: O(1)
         for(int i =0; i<nums.length; i++){
             int index = Math.abs(nums[i]);
             if(nums[index] < 0){
@@ -8,5 +10,26 @@ class Solution {
             nums[index] = -1 * nums[index];
         }
         return -1;
+        */
+        
+        //Solution 2: TC = O(N), SC: O(1)
+        // Floyd's Algorithm
+        int slow = 0, fast = 0;
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+                
+        int slow2 = 0;
+        
+        while(slow != slow2){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if(slow == slow2)
+                break;
+        }
+        
+        return slow;
     }
 }
