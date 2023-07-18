5fd48d0012e41f648df45bc82af5fb6417c8cdce
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-07-18 11:22:34 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (50.10%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrackDFS(res, new ArrayList<>(), 0, nums);
+        return res;
+    }
+    
+    public void backtrackDFS(List<List<Integer>> res, List<Integer> subset, int start, int[] nums){
+        if(start >= nums.length){
+            res.add(new ArrayList(subset));
+            return;
+        }
+        
+        subset.add(nums[start]);
+        backtrackDFS(res, subset, start + 1, nums);
+        
+        subset.remove(subset.size() - 1);
+        backtrackDFS(res, subset, start + 1, nums);
+    }
+}
