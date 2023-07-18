59a485fcb537df6cbc330f416f4fa451353adb71
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-07-18 11:41:53 -0700

    Time: 1 ms (100.00%), Space: 44 MB (52.70%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, new ArrayList<>(), 0, 0, target, candidates);
+        return res;
+    }
+    
+    public void dfs(List<List<Integer>> res, List<Integer> comb, int start, int sum, int target, int[] candidates){
+        if(sum == target){
+            res.add(new ArrayList(comb));
+            return;
+        }
+        
+        if(sum > target || start >= candidates.length)
+            return;
+        
+        for(int i = start; i < candidates.length; i++){
+            if(sum + candidates[i] > target)
+                continue;
+            comb.add(candidates[i]);
+            dfs(res, comb, i, sum + candidates[i], target, candidates);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
