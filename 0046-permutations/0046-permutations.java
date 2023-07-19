bc63eecd67a22999b2e88e635f4e437c837e6f80
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-07-19 15:47:56 -0700

    Time: 2 ms (48.05%), Space: 44.3 MB (5.06%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,4 +1,46 @@
 class Solution {
+    // NeetCode
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        // Base case
+        if (nums.length == 1) {
+            List<Integer> singlePermutation = new ArrayList<>();
+            singlePermutation.add(nums[0]);
+            res.add(singlePermutation);
+            return res;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int[] remainingNums = removeElement(nums, i);
+            List<List<Integer>> perms = permute(remainingNums);
+
+            for (List<Integer> perm : perms) {
+                perm.add(n);
+            }
+            res.addAll(perms);
+        }
+
+        return res;
+    }
+
+    private int[] removeElement(int[] nums, int index) {
+        int[] newArray = new int[nums.length - 1];
+        int newArrayIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index) {
+                newArray[newArrayIndex] = nums[i];
+                newArrayIndex++;
+            }
+        }
+
+        return newArray;
+    }    
+    
+    /* 
+    // Swapping method
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
@@ -22,4 +64,5 @@ class Solution {
             Collections.swap(perm, start, i);
         }
     }
+    */
 }
