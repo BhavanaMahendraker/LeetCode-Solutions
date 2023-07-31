2823af93c216391991313cd169a1911bc3594d27
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-07-31 16:31:07 -0700

    Time: 0 ms (100.00%), Space: 39.1 MB (76.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
+        /*
+        // TC: O(N), SC: O(N)
         int[] dp = new int[n+1];
         
         dp[n] = dp[n-1] = 1;
@@ -9,5 +11,16 @@ class Solution {
         }
         
         return dp[0];
+        */
+        
+        int first = 1, second = 1;
+        
+        for(int i=n - 2; i >= 0; i--){
+            int third = first + second;
+            second = first; 
+            first = third;
+        }
+        
+        return first;
     }
 }
