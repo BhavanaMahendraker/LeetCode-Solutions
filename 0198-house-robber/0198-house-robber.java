83d68abc0d23fa86097cfcc1b100440370381a7c
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-08-01 15:40:40 -0700

    Time: 0 ms (100.00%), Space: 39.9 MB (73.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i=2; i<dp.length; i++){
+            dp[i] = Math.max(nums[i-1] + dp[i-2], dp[i-1]);
+        }
+        
+        return dp[dp.length - 1];
+    }
+    
+    /* recursive stack
+    public int rob(int[] nums) {
         HashMap<Integer, Integer> memo = new HashMap<>();
         return dfs(nums, 0, memo);
     }
@@ -15,4 +27,5 @@ class Solution {
         memo.put(start, max);
         return max;
     }
+    */
 }
