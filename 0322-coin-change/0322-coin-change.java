942cc256051676bdc02e0bccdec9a8a0eacd35b5
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-08-03 06:39:21 -0700

    Time: 15 ms (88.69%), Space: 43.7 MB (34.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,47 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int res = dfs(coins, amount, 0, map);
-        return res == Integer.MAX_VALUE ? -1 : res;
+        
+        int [] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        dp[0] = 0;
+        
+        for(int i=0; i<=amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(i - coins[j] >=0 && dp[i - coins[j]] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+        return dp[dp.length - 1] == Integer.MAX_VALUE ? -1 : dp[dp.length - 1];
+    }
+    /*
+    public int coinChange(int[] coins, int amount) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        int minCoins = dfs(coins, amount, 0, memo);
+        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
     }
     
-    public int dfs(int[] coins, int amount, int start, HashMap<Integer, Integer> map){
+    public int dfs(int[] coins, int amount, int start, HashMap<Integer, Integer> memo){
         if(amount == 0){
             return 0;
         }
-        if(start >= coins.length || amount < 0){
+        if(amount < 0 || start >= coins.length){
             return Integer.MAX_VALUE;
         }
+        if(memo.containsKey(amount))
+            return memo.get(amount);
         
-        if(map.containsKey(amount))
-            return map.get(amount);
-        
-        int min = Integer.MAX_VALUE;
+        int minCoins = Integer.MAX_VALUE;
         
         for(int i=start; i<coins.length; i++){
-            int ans = dfs(coins, amount - coins[i], start, map);
-            if(ans != Integer.MAX_VALUE)
-                min = Math.min(min, 1 + ans);
+            int ans = dfs(coins, amount - coins[i], start, memo);
+            if(ans != Integer.MAX_VALUE){
+                minCoins =  Math.min(minCoins, 1 + ans);
+                memo.put(amount, minCoins);
+            }
         }
-        map.put(amount, min);
-        return map.get(amount);
+        
+        return minCoins;
     }
+    */
 }
