c0374f5f667545697b86796d3ab383f167cad732
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-08-04 00:26:38 -0700

    Time: 1 ms (99.98%), Space: 44.2 MB (79.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,15 +2,16 @@ class Solution {
     public boolean canJump(int[] nums) {
         boolean [] dp = new boolean[nums.length];
         dp[dp.length - 1] = true;
+        boolean reachable = false;
         int goal = dp.length - 1;
         
         for(int i=dp.length -2; i>= 0; i--){
-            if(i+nums[i] >= goal || dp[i + nums[i]] == true){
-                dp[i] = true;
+            if(i+nums[i] >= goal){
+                //dp[i] = true;
                 goal = i;
             }
         }
         
-        return dp[0];
+        return goal == 0;
     }
 }
