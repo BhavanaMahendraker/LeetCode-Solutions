6d8926c974f1c02a764c0df71adaf3355ddb6d0c
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-08-07 07:46:26 -0700

    Time: 29 ms (36.61%), Space: 43.8 MB (31.53%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> newMap = new HashMap<>();
+        int left =0;
+
+        for(int right = 0; right <s1.length(); right++){
+            map.put(s1.charAt(right), map.getOrDefault(s1.charAt(right), 0) + 1);
+        }
+
+        for(int right = 0; right < s2.length(); right++){
+            if(map.equals(newMap)) return true;
+            if(right >= s1.length()){
+                int decrement = newMap.get(s2.charAt(left)) -1;
+                if (decrement <= 0)
+                    newMap.remove(s2.charAt(left));
+                else
+                    newMap.put(s2.charAt(left), newMap.getOrDefault(s2.charAt(left), 0) -1);
+                left++;
+            }
+            newMap.put(s2.charAt(right), newMap.getOrDefault(s2.charAt(right), 0) + 1);
+        }
+        if(map.equals(newMap)) return true;
+
+        return false;
+    }
+}
