2ec1d57ae28d9439a8029c3b6428e3660b66e136
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2023-09-20 16:31:57 -0700

    Time: 5 ms (95.73%), Space: 44.4 MB (50.90%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,29 +1,22 @@
 class Solution {
-
     public List<String> findItinerary(List<List<String>> tickets) {
-        LinkedList<String> itinerary = new LinkedList<>();
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
-        Stack<String> stack = new Stack<>();
-
-        for (List<String> ticket : tickets) {
-            graph.putIfAbsent(ticket.get(0), new PriorityQueue<String>());
-            graph.get(ticket.get(0)).add(ticket.get(1));
+        HashMap<String, PriorityQueue<String>> adjList = new HashMap<>();
+        List<String> res = new ArrayList<>();
+        
+        for(List<String> t : tickets){
+            adjList.putIfAbsent(t.get(0), new PriorityQueue<>());
+            adjList.get(t.get(0)).add(t.get(1));
         }
-
-        stack.push("JFK");
-        while (!stack.isEmpty()) {
-            String nextDestination = stack.peek();
-
-            if (
-                !graph
-                    .getOrDefault(nextDestination, new PriorityQueue<>())
-                    .isEmpty()
-            ) {
-                stack.push(graph.get(nextDestination).poll());
-            } else {
-                itinerary.addFirst(stack.pop());
-            }
+        
+        dfs("JFK", adjList, res);
+        
+        return res;        
+    }
+    
+    public void dfs(String curr, HashMap<String, PriorityQueue<String>> adjList, List<String> res){
+        while(adjList.containsKey(curr) && !adjList.get(curr).isEmpty()){
+            dfs(adjList.get(curr).poll(), adjList, res);
         }
-        return itinerary;
+        res.add(0, curr);
     }
 }
