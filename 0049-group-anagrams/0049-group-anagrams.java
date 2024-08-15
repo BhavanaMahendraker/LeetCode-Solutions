1a2cc49347351f29e9c709f8c722237de02b1192
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-08-14 22:49:19 -0700

    Time: 17 ms (21.46%), Space: 48.1 MB (25.64%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
+        for(String str: strs){
+            int [] countArr = new int[26];
+            
+            for(char ch: str.toCharArray()){
+                countArr[ch - 'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for(int count: countArr){
+                sb.append('#').append(count);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList());
+            }
+            
+            map.get(key).add(str);
+        }
+        
+        return new ArrayList(map.values());
+    }
+}
