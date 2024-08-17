1fbeab20c467293417fd3bb4e02833aa8556c1ab
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-08-17 10:02:07 -0700

    Time: 11 ms (89.97%), Space: 49 MB (15.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,6 +1,37 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int num: nums){
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
         
+        ArrayList<Integer>[] bucket = new ArrayList[nums.length +1];
+                
+        for(Integer key: freq.keySet()){
+            int index = freq.get(key);
+            if(bucket[index] == null)
+                bucket[index] = new ArrayList<>();
+            bucket[freq.get(key)].add(key);
+        }
+        
+        int [] res = new int[k];
+        int pos = 0;
+        
+        for(int i = bucket.length - 1; i >= 0; i--){
+            if(bucket[i] != null){
+                for(int j=0; j < bucket[i].size(); j++){
+                    if(pos < k){
+                        res[pos] = bucket[i].get(j);
+                        pos++;
+                    }
+                }
+            }
+        }
+        
+        return res;
+        
+        /*
+        // TC: O(nlogn)
         HashMap<Integer, Integer> freq = new HashMap<>();
         
         for(int num: nums){
@@ -8,7 +39,6 @@ class Solution {
         }
         
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> freq.get(b) - freq.get(a));
-        
         pq.addAll(freq.keySet());
         
         int[] res = new int[k];
@@ -17,5 +47,6 @@ class Solution {
         }
         
         return res;
+        */
     }
 }
