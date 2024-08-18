b54d2c85fa32945ab56b76f38fd72216c3cea6fd
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-08-17 17:43:27 -0700

    Time: 1 ms (99.87%), Space: 55.1 MB (87.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,38 +1,42 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int prefix = 1, postfix = 1;
         int n = nums.length;
-        int[] leftArr = new int[n];
-        int[] rightArr = new int[n];
-        leftArr[0] = rightArr[nums.length-1] = 1;
+        int[] res = new int[nums.length];
         
-        for(int i=0; i<n-1; i++){
-            leftArr[i+1] = nums[i] * leftArr[i];
-        }
-        
-        for(int i=n-1; i>0; i--){
-            rightArr[i-1] = nums[i] * rightArr[i];
-        }
-     
         for(int i=0; i<n; i++){
-            rightArr[i] = leftArr[i] * rightArr[i];
+            res[i] = prefix;
+            prefix *= nums[i];
         }
         
+        for(int i=n-1; i>=0; i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
         
-        
-        return rightArr;
+        return res;
     }
 }
 
-
 /*
-[1,2,3,4]
- i
-[1,1,2,6] 
-       j
-       
-[24,12,4,1]
-       k
+1,2,3,4
+    i
 
- (j - 1) * i => j=i+1
- (k+1) * i
+prefix = 2 = res[i+1]
+
+res = 1,1,2,6
+            i+1
+
+res[i+1] = i * pre; (0-> n-1)
+
+
+postfix = 6 = res[i]
+
+res = 1,1,2,6
+        i
+          
+1,2,3,4
+    i+1
+
+res[i] = nums[i+1]*post;  i: n-2 -> 0
 */
