cdff8a61aebeeb1738ed33af801a8bbed15b4411
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-09-29 15:11:41 -0700

    Time: 14 ms (57.16%), Space: 48.7 MB (32.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,55 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> freqMap.get(b) - freqMap.get(a));
+        
         for(int num: nums){
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
         
-        ArrayList<Integer>[] bucket = new ArrayList[nums.length +1];
-                
-        for(Integer key: freq.keySet()){
-            int index = freq.get(key);
-            if(bucket[index] == null)
-                bucket[index] = new ArrayList<>();
-            bucket[freq.get(key)].add(key);
-        }
+        pq.addAll(freqMap.keySet());
         
         int [] res = new int[k];
-        int pos = 0;
-        
-        for(int i = bucket.length - 1; i >= 0; i--){
-            if(bucket[i] != null){
-                for(int j=0; j < bucket[i].size(); j++){
-                    if(pos < k){
-                        res[pos] = bucket[i].get(j);
-                        pos++;
-                    }
-                }
-                if(pos >= k){
-                    break;
-                }
-            }
-        }
-        
-        return res;
-        
-        /*
-        // TC: O(nlogn)
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        
-        for(int num: nums){
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> freq.get(b) - freq.get(a));
-        pq.addAll(freq.keySet());
-        
-        int[] res = new int[k];
-        for(int i =0; i<k; i++){
+        for(int i=0; i<k; i++){
             res[i] = pq.poll();
         }
         
         return res;
-        */
     }
 }
