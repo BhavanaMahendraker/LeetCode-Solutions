63b6d22d5c067574847d56d9027eb9924eeb9824
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-02 16:38:09 -0700

    Time: 28 ms (30.93%), Space: 44.1 MB (29.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,61 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> newMap = new HashMap<>();
-        int left =0;
-
-        for(int right = 0; right <s1.length(); right++){
-            map.put(s1.charAt(right), map.getOrDefault(s1.charAt(right), 0) + 1);
-        }
-
-        for(int right = 0; right < s2.length(); right++){
-            if(map.equals(newMap)) return true;
+        int left = 0, right = 0;
+        HashMap<Character, Integer> freqMapS1 = new HashMap<>();
+        HashMap<Character, Integer> freqMapS2 = new HashMap<>();
+        
+        for(char ch: s1.toCharArray()){
+            freqMapS1.put(ch, freqMapS1.getOrDefault(ch, 0)+1);
+        }     
+        
+        while(right < s2.length()){
+            char rightChar = s2.charAt(right);
+            freqMapS2.put(rightChar, freqMapS2.getOrDefault(rightChar, 0) + 1);
+            
             if(right >= s1.length()){
-                int decrement = newMap.get(s2.charAt(left)) -1;
-                if (decrement <= 0)
-                    newMap.remove(s2.charAt(left));
-                else
-                    newMap.put(s2.charAt(left), newMap.get(s2.charAt(left)) -1);
+                char leftChar = s2.charAt(left);
+                if(freqMapS2.get(leftChar) - 1 <= 0){
+                    freqMapS2.remove(leftChar);
+                } else{
+                    freqMapS2.put(leftChar, freqMapS2.get(leftChar) - 1);
+                }
                 left++;
             }
-            newMap.put(s2.charAt(right), newMap.getOrDefault(s2.charAt(right), 0) + 1);
+            right++;
+            
+            if(freqMapS1.equals(freqMapS2))
+                return true;
         }
-        if(map.equals(newMap)) return true;
-
+        
         return false;
     }
 }
+
+/*
+eidbaooo
+ l
+  r
+
+mapS2 =>
+e -> 1
+i -> 1
+
+mapS1 => 
+a -> 1
+b -> 1
+
+while r < S2 length
+    if(mapS1 == mapS2)
+        return true
+    r++
+    mapS2.put(rightChar, rightCharFreq++)
+    if(r >= mapS1.size())
+        mapS2.put(leftChar, leftCharFreq--)
+        if(leftCharFreq <= 0)
+            mapS2.remove(leftChar)
+        l++;
+
+return false;
+
+
+*/
