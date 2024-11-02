94323f3aee43ff6db990fe85474d1a3b36660a4b
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-02 14:58:30 -0700

    Time: 20 ms (38.21%), Space: 44.4 MB (22.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,13 +1,16 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int left = 0, right = 0, res = 0;
+        
+        // TC: O(N), SC: O(N)
+        int left = 0, right = 0, res = 0, maxFreq = 0;
         HashMap<Character, Integer> freqMap = new HashMap<>();
         
         while(right < s.length()){
             char rightChar = s.charAt(right);
             freqMap.put(rightChar, freqMap.getOrDefault(rightChar, 0) + 1);
             
-            while((right - left +  1) - Collections.max(freqMap.values()) > k){
+            maxFreq = Math.max(maxFreq, freqMap.get(rightChar));
+            while((right - left +  1) - maxFreq > k){
                 char leftChar = s.charAt(left);
                 freqMap.put(leftChar, freqMap.get(leftChar) - 1);
                 left++;
@@ -17,6 +20,26 @@ class Solution {
         }
         
         return res;
+        
+        
+        // TC: O(26N), SC: O(N) 
+//         int left = 0, right = 0, res = 0;
+//         HashMap<Character, Integer> freqMap = new HashMap<>();
+        
+//         while(right < s.length()){
+//             char rightChar = s.charAt(right);
+//             freqMap.put(rightChar, freqMap.getOrDefault(rightChar, 0) + 1);
+            
+//             while((right - left +  1) - Collections.max(freqMap.values()) > k){
+//                 char leftChar = s.charAt(left);
+//                 freqMap.put(leftChar, freqMap.get(leftChar) - 1);
+//                 left++;
+//             }
+//             res = Math.max(res, right - left + 1);
+//             right++;
+//         }
+        
+//         return res;
     }
 }
 
