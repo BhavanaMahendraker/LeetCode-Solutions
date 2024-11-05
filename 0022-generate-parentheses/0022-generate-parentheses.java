397072fef4178b9ce00fb113b4bc6dcc52ad0c17
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-05 07:22:20 -0800

    Time: 1 ms (84.40%), Space: 42.8 MB (84.30%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        helper(new StringBuilder(), 0, 0, n, res);
+        helper(res, new StringBuilder(), 0, 0, n);
         return res;
     }
     
-    private void helper(StringBuilder sb, int open, int close, int n, List<String> res){
-        if(sb.length() == n * 2){
-            res.add(new String(sb));
+    private void helper(List<String> res, StringBuilder comb, int open, int close, int n){
+        if(comb.length() == n * 2){
+            res.add(new String(comb));
             return;
         }
         
         if(open < n){
-            sb.append("(");
-            helper(sb, open + 1, close, n, res);
-            sb.deleteCharAt(sb.length() - 1);
+            comb.append("(");
+            helper(res, comb, open + 1, close, n);
+            comb.deleteCharAt(comb.length() - 1);
         }
         
         if(close < open){
-            sb.append(")");
-            helper(sb, open, close + 1, n, res);
-            sb.deleteCharAt(sb.length() - 1);
+            comb.append(")");
+            helper(res, comb, open, close + 1, n);
+            comb.deleteCharAt(comb.length() - 1);
         }
     }
 }
