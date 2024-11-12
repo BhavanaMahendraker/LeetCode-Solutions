502a4d836cb4e49f56610c074ca03cb32dd4e88c
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-11 20:54:43 -0800

    Time: 0 ms (100.00%), Space: 42.6 MB (8.27%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,23 +1,41 @@
 class Solution {
     public int findMin(int[] nums) {
-        int left = 0, right = nums.length - 1, res = nums[0];
-        
-        while(left <= right){
-            if(nums[left] <= nums[right]){
-                return Math.min(res, nums[left]);
-            }
-            
+        // NOTE: With finding exact pivot
+        int left = 0, right = nums.length - 1;
+        while(left < right){
+            if(nums[left] < nums[right])
+                return nums[left];
             int mid = left + (right - left) / 2;
-            res = Math.min(res, nums[mid]);
-            
-            if(nums[mid] > nums[right]){
+            if(nums[mid] > nums[right])
                 left = mid + 1;
-            } else if(nums[left] > nums[mid]){
-                right = mid - 1;
-            }
+            else if (nums[left] > nums[mid])
+                right = mid;
         }
         
-        return res;
+        return nums[left];
+
+
+// -------------------------------------------------------------
+        // NEETCODE SOLUTION
+        // NOTE: Without finding exact pivot
+        
+//         int left = 0, right = nums.length - 1, res = nums[0];
+//         while(left <= right){
+//             if(nums[left] <= nums[right]){
+//                 return Math.min(res, nums[left]);
+//             }
+            
+//             int mid = left + (right - left) / 2;
+//             res = Math.min(res, nums[mid]);
+            
+//             if(nums[mid] > nums[right]){
+//                 left = mid + 1;
+//             } else if(nums[left] > nums[mid]){
+//                 right = mid - 1;
+//             }
+//         }
+        
+//         return res;
     }
 }
 
@@ -36,6 +54,7 @@ mVal > rVal
     l = m + 1
 
 lVal > mVal
-    r = m -1
+    r = m -1 (when not finding exact pivot)
+    r = m (when finding exact pivot)
 
 */
