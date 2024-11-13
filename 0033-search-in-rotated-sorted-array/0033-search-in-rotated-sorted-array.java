414ed76690d67951e2551254f06bb7e3a4dc87a0
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-12 21:33:58 -0800

    Time: 0 ms (100.00%), Space: 42.3 MB (21.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,29 +3,50 @@ class Solution {
         int left = 0, right = nums.length - 1;
         
         while(left <= right){
-            int mid = (left + right) / 2;
+            int mid = left + (right - left)/2;
             
-            if(nums[mid] == target) 
+            if(nums[mid] == target)
                 return mid;
             
-            // Left portion of the array
             if(nums[left] <= nums[mid]){
-                if(target < nums[left] || target > nums[mid])
+                if(target < nums[left] || nums[mid] < target){
                     left = mid + 1;
-                else
-                    right = mid -1;                    
-            }
-            
-            // Right portion of the array
-            else{
-                if(target > nums[right] || target < nums[mid])
+                } else{
                     right = mid - 1;
-                else
+                }
+            } else{
+                if(nums[right] < target || target < nums[mid]){
+                    right = mid - 1;
+                } else{
                     left = mid + 1;
+                }
             }
-            
         }
         
         return -1;
     }
 }
+
+/*
+4,5,6,7,0,1,2
+      m
+l
+            r
+            
+nums[m] == target
+    return m
+
+nums[l] <= nums[m]{
+    target < nums[left] || nums[m] < target
+        l = m + 1
+    else
+        r = m - 1
+
+} else{
+    nums[r] < target || nums[m] < target
+        r = m - 1
+    else
+        l = m + 1
+}
+
+*/
