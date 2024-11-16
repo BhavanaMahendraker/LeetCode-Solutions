8539f2dc9ea183a9bf89872a204cc253e8a7437d
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-16 12:32:03 -0800

    Time: 163 ms (16.92%), Space: 120 MB (48.30%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,42 +1,40 @@
 class TimeMap {
-
-    HashMap<String, List<Pair<String, Integer>>> map;
+    
+    HashMap<String, ArrayList<Pair<Integer, String>>> timeMap;
 
     public TimeMap() {
-        map = new HashMap<>();
+        timeMap = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new ArrayList<>());
-        map.get(key).add(new Pair(value, timestamp));
-    }
-
-    public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) 
-            return "";
-        List<Pair<String, Integer>> list = map.get(key);
-        return search(list, timestamp);
-    }
-
-    public String search(List<Pair<String, Integer>> list, int timestamp) {
-        int start = 0;
-        int end = list.size() - 1;
-        String res = "";
-        while (start <= end) {
-            int mid = start + (end - start + 1) / 2;
-            if(list.get(mid).getValue() == timestamp)
-                return list.get(mid).getKey();
-            if (list.get(mid).getValue() < timestamp) {
-                start = mid + 1; 
-                res = list.get(mid).getKey();
-            }
-            else 
-                end = mid - 1;
+        if(!timeMap.containsKey(key)){
+            timeMap.put(key, new ArrayList<>());
         }
+        
+        timeMap.get(key).add(new Pair<>(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        String res = "";
+        
+        if(!timeMap.containsKey(key))
+            return res;
+        
+        int left = 0, right = timeMap.get(key).size() - 1;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(timeMap.get(key).get(mid).getKey() <= timestamp){
+                res = timeMap.get(key).get(mid).getValue();
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+        
         return res;
     }
 }
-
 
 /**
  * Your TimeMap object will be instantiated and called as such:
