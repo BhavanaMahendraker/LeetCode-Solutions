63dd553e468c5ffdac07f4259b50438cf2a837ac
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-16 20:00:49 -0800

    Time: 0 ms (100.00%), Space: 42.8 MB (16.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,42 +10,28 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode p = list1, q = list2, res = new ListNode(), dummy = res;
+        ListNode res = new ListNode(), dummy = res;
         
-        while(p != null || q!= null){
-            int pVal = p == null ? Integer.MAX_VALUE : p.val;
-            int qVal = q == null ? Integer.MAX_VALUE : q.val;
-            
-            if(pVal <= qVal){
-                res.next = p;
-                p = p.next;
+        while(list1 != null && list2!= null){
+            if(list1.val <= list2.val){
+                res.next = list1;
+                list1 = list1.next;
             } else{
-                res.next = q;
-                q = q.next;
+                res.next = list2;
+                list2 = list2.next;
             }
             res = res.next;
+        }
+        
+        if(list1 != null){
+            res.next = list1;
+        }
+        
+        if(list2 != null){
+            res.next = list2;
         }
         
         return dummy.next;
         
     }
 }
-
-/*
-1,2,4,5,6,7,8
-p
-
-
-1,3,4
-q
-
-pVal = pVal == null ? 0 : p.val
-qVal = qVal == null ? 0 : q.val
-
-pVal <=qVal
-    res.next() = p
-    p = p.next()
-else
-    res.next= q
-    q = q.next
-*/
