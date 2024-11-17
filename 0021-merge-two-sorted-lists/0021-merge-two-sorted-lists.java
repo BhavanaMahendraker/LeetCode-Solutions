045d5252f5d87c4efbe0ea885f12c896aa02291b
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-16 20:14:30 -0800

    Time: 0 ms (100.00%), Space: 42.6 MB (25.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,28 +10,50 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode res = new ListNode(), dummy = res;
         
-        while(list1 != null && list2!= null){
-            if(list1.val <= list2.val){
-                res.next = list1;
-                list1 = list1.next;
-            } else{
-                res.next = list2;
-                list2 = list2.next;
-            }
-            res = res.next;
+        // Recursive Solution
+        // Base Case
+        if(list1 == null){
+            return list2;
         }
         
-        if(list1 != null){
-            res.next = list1;
+        if(list2 == null){
+            return list1;
         }
         
-        if(list2 != null){
-            res.next = list2;
+        // Recursive call
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
         
-        return dummy.next;
+//         // Non-Recursive Solution
+        
+//         ListNode res = new ListNode(), dummy = res;
+        
+//         while(list1 != null && list2!= null){
+//             if(list1.val <= list2.val){
+//                 res.next = list1;
+//                 list1 = list1.next;
+//             } else{
+//                 res.next = list2;
+//                 list2 = list2.next;
+//             }
+//             res = res.next;
+//         }
+        
+//         if(list1 != null){
+//             res.next = list1;
+//         }
+        
+//         if(list2 != null){
+//             res.next = list2;
+//         }
+        
+//         return dummy.next;
         
     }
 }
