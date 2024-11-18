54d66cfc36459da210ebd432a7e689dd8414afe4
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-17 21:27:27 -0800

    Time: 0 ms (100.00%), Space: 44.4 MB (43.42%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,19 +15,69 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node, Node> mapToCopy = new HashMap<>();
-        Node p = head;
+        if(head == null) return null;
+        Node currNode = head;
         
-        while(p != null){
-            mapToCopy.put(p, new Node(p.val));
-            p = p.next;
+        // Creating new nodes
+        while(currNode != null){
+            Node newNode = new Node(currNode.val);
+            Node next = currNode.next;
+            currNode.next = newNode;
+            newNode.next = next;
+            currNode = next;
         }
         
-        for(Node node: mapToCopy.keySet()){
-            mapToCopy.get(node).next = mapToCopy.get(node.next);
-            mapToCopy.get(node).random = mapToCopy.get(node.random);
+        // Setting random
+        currNode = head;
+        while(currNode != null){
+            if(currNode.random != null){
+                currNode.next.random = currNode.random.next;
+            }
+            currNode = currNode.next.next;
         }
         
-        return mapToCopy.get(head);
+        // Separating new Nodes from original
+        currNode = head;
+        Node newHead = head.next;
+        while(currNode != null){
+            Node nextNode = currNode.next.next;
+            currNode.next.next = nextNode == null ? null : nextNode.next;
+            currNode.next = nextNode;
+            currNode = nextNode;
+        }
+        
+        return newHead;
+        
+        
+        
+        
+//         HashMap<Node, Node> mapToCopy = new HashMap<>();
+//         Node p = head;
+        
+//         while(p != null){
+//             mapToCopy.put(p, new Node(p.val));
+//             p = p.next;
+//         }
+        
+//         for(Node node: mapToCopy.keySet()){
+//             mapToCopy.get(node).next = mapToCopy.get(node.next);
+//             mapToCopy.get(node).random = mapToCopy.get(node.random);
+//         }
+        
+//         return mapToCopy.get(head);
     }
 }
+
+/*
+[7,null],[13,0],[11,4],[10,2],[1,0]
+                                    p
+ 
+[7,null],[13,null],[11,nul],[10,null][1,null]
+ 
+next = p.next
+p.next = newNode
+newNode.next = next
+
+
+
+*/
