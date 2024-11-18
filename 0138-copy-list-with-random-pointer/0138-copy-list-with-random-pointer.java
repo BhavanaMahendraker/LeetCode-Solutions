433621179110724710b246429e92b2bbb02af5a3
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-17 19:43:04 -0800

    Time: 1 ms (8.28%), Space: 44.7 MB (13.68%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,65 +15,22 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        /*
-        // TC = O(N); SC = O(N)
+        HashMap<Node, Node> map = new HashMap<>();
+        Node p = head;
         
-        HashMap<Node, Node> mapToCopy = new HashMap<>();
-        Node p1 = head;
-        
-        // Create map with copy nodes
-        while(p1 != null){
-            mapToCopy.put(p1, new Node(p1.val));
-            p1 = p1.next;
+        while(p != null){
+            map.put(p, new Node(p.val));
+            p = p.next;
         }
         
-        // Connect the nodes
-        p1 = head;
-        
-        while(p1 != null){
-            mapToCopy.get(p1).next = mapToCopy.get(p1.next);
-            mapToCopy.get(p1).random = mapToCopy.get(p1.random);
-            p1 = p1.next;
-        }
-        
-        return mapToCopy.get(head);
-        */
-        
-        // TC = O(N); SC = O(1)
-        if(head == null) 
-            return null;
-        Node p1 = head;
-        while(p1 != null){
-            Node p1_next = p1.next;
-            Node copy_node = new Node(p1.val);
-            p1.next = copy_node;
-            copy_node.next = p1_next;
-            p1 = p1_next;
-        }
-        // Connect random nodes
-        p1 = head;
-        
-        while(p1 != null){
-            if(p1.random != null){
-                p1.next.random = p1.random.next;
-            }
-            p1 = p1.next.next;
-        }
-        
-        // Restore next pointers
-        p1 = head;
-        Node copy_head = head.next;
-        
-        while(p1 != null){
-            Node copy_node = p1.next;
-            Node next_node = p1.next.next;
-            p1.next = next_node;
+        for(Node node: map.keySet()){
+            Node nextNode = node.next;
+            map.get(node).next = map.get(nextNode);
             
-            if(next_node != null)
-                copy_node.next = next_node.next;
-            p1 = next_node;
+            Node randomNode = node.random;
+            map.get(node).random = map.get(randomNode);
         }
         
-        return copy_head;
+        return map.get(head);
     }
 }
