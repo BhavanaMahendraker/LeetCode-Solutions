9871c4dfad5827e6ca8e7a947d854ac6a0014340
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-17 21:51:26 -0800

    Time: 1 ms (100.00%), Space: 44.7 MB (23.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,31 +10,41 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p = l1, q = l2, head = new ListNode(-1);
-        ListNode cur = head;
         int carry = 0;
+        ListNode head = new ListNode(-1), currNode = head;
         
-        while(p != null || q != null){
-            int val1 = p == null ? 0 : p.val;
-            int val2 = q == null ? 0 : q.val;
+        while(l1 != null || l2 != null){
+            int l1Val = l1 == null ? 0 : l1.val; // 9
+            int l2Val = l2 == null ? 0 : l2.val; // 9
             
-            int curr_val = (val1 + val2 + carry);
-            carry = curr_val / 10;
+            int currVal = l1Val + l2Val + carry; // 18
+            currNode.next = new ListNode(currVal % 10); // 8
+            carry = currVal / 10; // 1
             
-            ListNode node = new ListNode(curr_val % 10, null);
-            cur.next = node;
-            
-            p = p == null ? null : p.next;
-            q = q == null ? null : q.next;
-            cur = cur.next;
+            currNode = currNode.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
         
         if(carry != 0){
-            ListNode node = new ListNode(carry, null);
-            cur.next = node;
-            cur = cur.next;
+            currNode.next = new ListNode(carry);
         }
-                
+        
         return head.next;
     }
 }
+
+/*
+[2,4,3]
+[5,6,7,9,9,9]
+[7,0,1,0,0,0,1]
+
+l1 or 0 + l2 or 0 + carry
+store mod in currNode 10 -> 0
+carry = 1
+next....
+
+if carry != 0
+    store carry
+
+*/
