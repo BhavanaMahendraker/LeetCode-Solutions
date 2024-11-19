2438c4ea2acd71cd98c9bddac98a7beeacd0eeec
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-19 13:51:45 -0800

    Time: 0 ms (100.00%), Space: 44.2 MB (66.25%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,26 +11,12 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        /*
-        // Solution 1: TC = O(N), SC = O(N)
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode p = head;
-        
-        while(p != null){
-            if(set.contains(p)) return true;
-            set.add(p);
-            p = p.next;
-        }
-        
-        return false;
-        */
-        
-        // Solution 1:  TC = O(N), SC = O(1) 
         ListNode slow = head, fast = head;
         
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
+            
             if(slow == fast)
                 return true;
         }
