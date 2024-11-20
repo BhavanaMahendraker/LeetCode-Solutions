2289f2ff8edcbf984ffdbdd68f41482a77644531
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-20 15:51:10 -0800

    Time: 2 ms (84.33%), Space: 44.7 MB (25.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,35 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        dfs(res, new ArrayList<>(), 0, 0, target, candidates);
+        dfs(res, new ArrayList<>(), candidates, target, 0);
         return res;
     }
     
-    public void dfs(List<List<Integer>> res, List<Integer> comb, int start, int sum, int target, int[] candidates){
-        if(sum == target){
-            res.add(new ArrayList(comb));
+    public void dfs(List<List<Integer>> res, List<Integer> comb, int[] candidates, int target, int start){
+        if(target < 0 || start >= candidates.length){
             return;
         }
         
-        if(sum > target || start >= candidates.length)
-            return;
+        if(target == 0){
+            res.add(new ArrayList(comb));
+        }
         
-        // Neetcode
-        comb.add(candidates[start]);
-        dfs(res, comb, start, sum + candidates[start], target, candidates);
-        comb.remove(comb.size() - 1);
-        
-        dfs(res, comb, start + 1, sum, target, candidates);
-        
-        /*
-        // loop solution 
-        for(int i = start; i < candidates.length; i++){
-            if(sum + candidates[i] > target)
-                continue;
+        for(int i=start; i<candidates.length; i++){
             comb.add(candidates[i]);
-            dfs(res, comb, i, sum + candidates[i], target, candidates);
+            dfs(res, comb, candidates, target - candidates[i], i);
             comb.remove(comb.size() - 1);
         }
-        */
     }
 }
