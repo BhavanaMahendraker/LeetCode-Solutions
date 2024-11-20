bb0a23b9f699d684393c7f7ceba909b2815e90c1
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-20 12:36:29 -0800

    Time: 1 ms (33.58%), Space: 44.1 MB (76.14%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,45 +15,24 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        /* HashSet In-Order method
+        // Inorder Traversal with Iteration
         Stack<TreeNode> stack = new Stack<>();
-        HashSet<TreeNode> visited = new HashSet<>();
-        stack.push(root);
-        visited.add(root);
+        TreeNode curr = root;
         
-        while(!stack.isEmpty()){
-            while(stack.peek().left != null && !visited.contains(stack.peek().left)){
-                TreeNode left = stack.peek().left;
-                visited.add(left);
-                stack.push(left);
-            }
-            TreeNode curr = stack.pop();
-            k--;
-            if(k == 0)
-                return curr.val;
-            if(curr.right != null)
-                stack.push(curr.right);
-        }
-        return -1;
-        */
-        
-        // Inorder traversal
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root; // pointer
-        
-        while(k >= 0){
+        while(!stack.isEmpty() || curr != null){
             while(curr != null){
                 stack.push(curr);
                 curr = curr.left;
             }
             curr = stack.pop();
             k--;
-            if(k == 0)
+            if(k == 0){
                 return curr.val;
-            curr = curr.right; // Point at right (even if null)
-                                // but push to stack in next iteration
+            }
+            
+            curr = curr.right;
         }
-        return -1;
         
+        return -1;
     }
 }
