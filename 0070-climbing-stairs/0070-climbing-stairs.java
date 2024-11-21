51f4961abffed7c0347fbbcd2be542522f71f303
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-20 21:29:15 -0800

    Time: 0 ms (100.00%), Space: 40.3 MB (42.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,36 @@
 class Solution {
     public int climbStairs(int n) {
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        return dfs(n, 0, memo);
+        // HashMap<Integer, Integer> memo = new HashMap<>();
+        // return dfs(n, 0, memo);
+        
+        return dpSol(n);
     }
     
-    private int dfs(int n, int i, HashMap<Integer, Integer> memo){
+    private int dpSol(int n){
+        int[] dp = new int[n+2];
+        dp[n+1] = 0;
+        dp[n] = 1;
         
-        if(i > n)
-            return 0;
+        for(int i = n-1; i>= 0; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
         
-        if(i == n)
-            return 1;
-        
-        if(memo.containsKey(i))
-            return memo.get(i);
-        
-        int numOfWays = dfs(n, i+1, memo) + dfs(n, i+2, memo);
-        memo.put(i, numOfWays);
-        
-        return memo.get(i);
+        return dp[0];
     }
+    
+//     private int dfs(int n, int i, HashMap<Integer, Integer> memo){
+//         if(i > n)
+//             return 0;
+        
+//         if(i == n)
+//             return 1;
+        
+//         if(memo.containsKey(i))
+//             return memo.get(i);
+        
+//         int numOfWays = dfs(n, i+1, memo) + dfs(n, i+2, memo);
+//         memo.put(i, numOfWays);
+        
+//         return memo.get(i);
+//     }
 }
