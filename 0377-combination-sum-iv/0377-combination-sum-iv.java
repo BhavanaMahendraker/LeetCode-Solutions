121cbe71cc5bdba688572c67c0ae87a87a0a2eda
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-22 21:09:44 -0800

    Time: 2 ms (32.04%), Space: 41.2 MB (28.19%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,7 +1,8 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         // return dfs(nums, target, 0);
-        return memoization(nums, target, 0, new HashMap<>());
+        // return memoization(nums, target, 0, new HashMap<>());
+        return dp(nums, target);
     }
     
     public int dfs(int[]nums, int target, int index){
@@ -37,5 +38,21 @@ class Solution {
         memo.put(target, including + excluding);
         
         return memo.get(target);
+    }
+    
+    public int dp(int[]nums, int target){
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for(int i=0; i<=target; i++){
+            for(int j=0; j< nums.length; j++){
+                if(i >= nums[j]){
+                    dp[i] = dp[i-nums[j]] + dp[i];
+                }
+            }
+        }
+        
+        return dp[target];
+        
     }
 }
