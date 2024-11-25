7f98f40d333a9e8e4747bcf5f8bac4ee3aa8dd6c
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-24 18:01:35 -0800

    Time: 164 ms (5.07%), Space: 46.8 MB (5.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,47 +1,50 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // int res = solveRec(coins, amount);
+        int res = solveMemo(coins, amount, new HashMap<>());
         
-        int [] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        
-        dp[0] = 0;
-        
-        for(int i=0; i<=amount; i++){
-            for(int j=0; j<coins.length; j++){
-                if(i - coins[j] >=0 && dp[i - coins[j]] != Integer.MAX_VALUE)
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-            }
-        }
-        return dp[dp.length - 1] == Integer.MAX_VALUE ? -1 : dp[dp.length - 1];
-    }
-    /*
-    public int coinChange(int[] coins, int amount) {
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        int minCoins = dfs(coins, amount, 0, memo);
-        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
     
-    public int dfs(int[] coins, int amount, int start, HashMap<Integer, Integer> memo){
+    public int solveMemo(int[] coins, int amount, HashMap<Integer, Integer> memo){
         if(amount == 0){
             return 0;
         }
-        if(amount < 0 || start >= coins.length){
+        
+        if(amount < 0){
             return Integer.MAX_VALUE;
         }
+        
         if(memo.containsKey(amount))
             return memo.get(amount);
         
         int minCoins = Integer.MAX_VALUE;
+        for(int i=0; i<coins.length; i++){
+            int curr = solveMemo(coins, amount - coins[i], memo);
+            if(curr != Integer.MAX_VALUE)
+                minCoins = Math.min(minCoins, curr + 1);
+        }
         
-        for(int i=start; i<coins.length; i++){
-            int ans = dfs(coins, amount - coins[i], start, memo);
-            if(ans != Integer.MAX_VALUE){
-                minCoins =  Math.min(minCoins, 1 + ans);
-                memo.put(amount, minCoins);
-            }
+        memo.put(amount, minCoins);
+        return memo.get(amount);
+    }
+    
+    public int solveRec(int[] coins, int amount){
+        if(amount == 0){
+            return 0;
+        }
+        
+        if(amount < 0){
+            return Integer.MAX_VALUE;
+        }
+        
+        int minCoins = Integer.MAX_VALUE;
+        for(int i=0; i<coins.length; i++){
+            int curr = solveRec(coins, amount - coins[i]);
+            if(curr < Integer.MAX_VALUE)
+                minCoins = Math.min(minCoins, curr + 1);
         }
         
         return minCoins;
     }
-    */
 }
