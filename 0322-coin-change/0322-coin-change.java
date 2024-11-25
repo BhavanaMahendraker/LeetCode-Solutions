6b4ef28d46781cffa9ce68b88517af3fe659fea3
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-24 18:09:20 -0800

    Time: 17 ms (45.27%), Space: 44.2 MB (73.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,11 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         // int res = solveRec(coins, amount);
-        int res = solveMemo(coins, amount, new HashMap<>());
+        // int res = solveMemo(coins, amount, new HashMap<>());
+        int res = solveDP(coins, amount);
         
         return res == Integer.MAX_VALUE ? -1 : res;
     }
     
+    public int solveDP(int[] coins, int amount){
+        int[] dp = new int[amount+1];
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for(int i = 1; i<=amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        
+        return dp[amount];
+    }
+        
     public int solveMemo(int[] coins, int amount, HashMap<Integer, Integer> memo){
         if(amount == 0){
             return 0;
