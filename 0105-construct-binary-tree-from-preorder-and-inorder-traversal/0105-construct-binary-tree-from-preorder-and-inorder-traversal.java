86aa2286420ee448c07e161b3806369a7a92e469
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-27 20:41:34 -0800

    Time: 3 ms (38.04%), Space: 44.2 MB (68.47%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,6 +15,34 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // return neetcodeSolution(preorder, inorder);
+        return loveBabbarSolution(preorder, inorder, new int[]{0}, 0, inorder.length - 1);
+    }
+    
+    private TreeNode loveBabbarSolution(int[] preorder, int[] inorder, 
+                                        int[] preorderIndex, int inorderStart, int inorderEnd) {
+        if(preorderIndex[0] >= preorder.length || inorderStart > inorderEnd){
+            return null;
+        }
+        
+        int currElement = preorder[preorderIndex[0]++];
+        TreeNode root = new TreeNode(currElement);
+        
+        int mid = -1;
+        for(int i=0; i<preorder.length; i++){
+            if(inorder[i] == currElement){
+                mid = i;
+                break;
+            }
+        }
+        
+        root.left = loveBabbarSolution(preorder, inorder, preorderIndex, inorderStart, mid - 1);
+        root.right = loveBabbarSolution(preorder, inorder, preorderIndex, mid + 1, inorderEnd);
+        
+        return root;        
+    }
+    
+    private TreeNode neetcodeSolution(int[] preorder, int[] inorder) {
         if(preorder.length == 0 && inorder.length == 0){
             return null;
         }
