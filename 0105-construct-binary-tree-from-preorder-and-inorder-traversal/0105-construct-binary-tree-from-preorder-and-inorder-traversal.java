b66c7ae6d45ced65596b6b666b449443301c2cab
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-27 21:06:54 -0800

    Time: 3 ms (38.04%), Space: 44.1 MB (79.12%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,34 +17,11 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         // return neetcodeSolution(preorder, inorder);
         // return loveBabbarSolution(preorder, inorder, new int[]{0}, 0, inorder.length - 1);
-        return neetcodeSolutionOptimized(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        return myBestUnderStoodSolution(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
         
     }
     
-    private TreeNode loveBabbarSolution(int[] preorder, int[] inorder, 
-                                        int[] preorderIndex, int inorderStart, int inorderEnd) {
-        if(preorderIndex[0] >= preorder.length || inorderStart > inorderEnd){
-            return null;
-        }
-        
-        int currElement = preorder[preorderIndex[0]++];
-        TreeNode root = new TreeNode(currElement);
-        
-        int mid = -1;
-        for(int i=0; i<preorder.length; i++){
-            if(inorder[i] == currElement){
-                mid = i;
-                break;
-            }
-        }
-        
-        root.left = loveBabbarSolution(preorder, inorder, preorderIndex, inorderStart, mid - 1);
-        root.right = loveBabbarSolution(preorder, inorder, preorderIndex, mid + 1, inorderEnd);
-        
-        return root;        
-    }
-    
-    private TreeNode neetcodeSolutionOptimized(int[] preorder, int[] inorder, 
+    private TreeNode myBestUnderStoodSolution(int[] preorder, int[] inorder, 
                                                int preStart, int preEnd, int inStart, int inEnd) {
         if(preStart > preEnd || inStart > inEnd){
             return null;
@@ -59,9 +36,13 @@ class Solution {
                 break;
             }
         }
-        
-        root.left = neetcodeSolutionOptimized(preorder, inorder, preStart+1, preStart + mid - inStart, inStart, mid - 1);
-        root.right = neetcodeSolutionOptimized(preorder, inorder, preStart + mid - inStart + 1, preEnd, mid+1, inEnd);
+        // preStart + mid - inStart + 1 ==> Gives the index of the starting point of right half in preorder array
+        root.left = myBestUnderStoodSolution(preorder, inorder, 
+                                              preStart+1, preStart + mid - inStart, 
+                                              inStart, mid - 1);
+        root.right = myBestUnderStoodSolution(preorder, inorder, 
+                                               preStart + mid - inStart + 1, preEnd, 
+                                               mid+1, inEnd);
         
         return root;
     }
@@ -88,4 +69,28 @@ class Solution {
         
         return root;
     }
+    
+    private TreeNode loveBabbarSolution(int[] preorder, int[] inorder, 
+                                        int[] preorderIndex, int inorderStart, int inorderEnd) {
+        if(preorderIndex[0] >= preorder.length || inorderStart > inorderEnd){
+            return null;
+        }
+        
+        int currElement = preorder[preorderIndex[0]++];
+        TreeNode root = new TreeNode(currElement);
+        
+        int mid = -1;
+        for(int i=0; i<preorder.length; i++){
+            if(inorder[i] == currElement){
+                mid = i;
+                break;
+            }
+        }
+        
+        root.left = loveBabbarSolution(preorder, inorder, preorderIndex, inorderStart, mid - 1);
+        root.right = loveBabbarSolution(preorder, inorder, preorderIndex, mid + 1, inorderEnd);
+        
+        return root;        
+    }
+
 }
