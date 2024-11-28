6573f19ca067739510acd9ce0bddb0b856e6983d
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-27 22:14:59 -0800

    Time: 138 ms (71.09%), Space: 41.8 MB (33.32%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean [][] visited = new boolean[board.length][board[0].length];
-        for(int i=0; i<board.length; i++){
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        
+        for(int i = 0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(dfs(board, word, visited, 0, i, j))
+                if(dfs(board, word, visited, i, j, 0)){
                     return true;
+                }
             }
         }
+        
         return false;
     }
     
-    public boolean dfs(char[][] board, String word, boolean[][] visited, int curr, int i, int j){
-        if(curr >= word.length())
+    private boolean dfs(char[][] board, String word, boolean[][] visited, int i, int j, int letterIndex){
+        if(letterIndex == word.length())
             return true;
         
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length 
-           || board[i][j] != word.charAt(curr) || visited[i][j])
+        if(i<0 || j<0 || i>=board.length || j>=board[i].length 
+           || word.charAt(letterIndex) != board[i][j] || visited[i][j] == true)
             return false;
+        
         
         visited[i][j] = true;
         
-        boolean res =  dfs(board, word, visited, curr+1, i+1, j)
-                    || dfs(board, word, visited, curr+1, i-1, j)
-                    || dfs(board, word, visited, curr+1, i, j+1)
-                    || dfs(board, word, visited, curr+1, i, j-1);
-        visited[i][j] = false;
+        boolean ans = dfs(board, word, visited, i+1, j, letterIndex + 1)
+                    || dfs(board, word, visited, i-1, j, letterIndex + 1)
+                    || dfs(board, word, visited, i, j+1, letterIndex + 1)
+                    || dfs(board, word, visited, i, j-1, letterIndex + 1);
         
-        return res;
+        visited[i][j] = false;
+
+        return ans;
     }
 }
