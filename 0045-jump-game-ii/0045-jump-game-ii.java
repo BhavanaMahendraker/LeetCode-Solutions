d01837a97b1ccb09ca70b4cb5a12f3bd49f4883a
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-28 18:51:26 -0800

    Time: 1 ms (99.17%), Space: 45.5 MB (22.10%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int [] dp = new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[dp.length -1] = 0;
-        
-        for(int i=dp.length-2; i>= 0; i--){
-            int jumpLen = nums[i];
-            while(jumpLen > 0){
-                if(i + jumpLen < nums.length && dp[i + jumpLen] != Integer.MAX_VALUE)
-                    dp[i] = Math.min(dp[i], 1 + dp[i + jumpLen]);
-                jumpLen--;
+        int res = 0, l = 0, r = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
             }
+            l = r + 1;
+            r = farthest;
+            res++;
         }
-        return dp[0] == Integer.MAX_VALUE ? -1 : dp[0];
+        return res;
     }
 }
