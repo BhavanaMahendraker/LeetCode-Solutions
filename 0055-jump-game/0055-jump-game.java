2707f75abe43bfa454a5c5d33351eafb295872ca
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-11-28 16:19:31 -0800

    Time: 2 ms (79.93%), Space: 45.4 MB (84.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,15 +2,21 @@ class Solution {
     public boolean canJump(int[] nums) {
         boolean [] res = new boolean[nums.length];
         res[nums.length - 1] = true;
+        int goal = nums.length - 1;
         
         for(int i=res.length-2; i>=0; i--){
-            int jumpLength = nums[i];
-            for(int j=0; j<=Math.min(nums.length, jumpLength); j++){
-                if(res[i + j] == true){
-                    res[i] = true;
-                    break;
-                }
+            if(i+nums[i] >= goal){
+                res[i] = true;
+                goal = i;
             }
+            
+            // int jumpLength = nums[i];
+            // for(int j=0; j<=Math.min(nums.length, jumpLength); j++){
+            //     if(res[i + j] == true){
+            //         res[i] = true;
+            //         break;
+            //     }
+            // }
         }
         
         return res[0];
