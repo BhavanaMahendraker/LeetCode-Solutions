4359b86c250aa3936a5173e047f688e57be2069c
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-03 23:24:32 -0800

    Time: 0 ms (100.00%), Space: 40.5 MB (40.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,45 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        dp[0][0] = 0;
-        
-        for(int i=0; i<n; i++){
-            dp[0][i] = 1;
+        // return solveRec(m, n, 0, 0, new boolean[m][n]);
+        int[][] memo = new int[m][n];
+        for(int[] row: memo){
+            Arrays.fill(row, -1);
         }
+        return solveMem(m, n, 0, 0, new boolean[m][n], memo);
+    }
+    
+    private int solveMem(int m, int n, int i, int j, boolean[][]visited, int[][] memo){
+        if(i<0 || j<0 || i>=m || j>=n || visited[i][j] == true)
+            return 0;
         
-        for(int i=0; i<m; i++){
-            dp[i][0] = 1;
-        }
+        if(i == m-1 && j==n-1)
+            return 1;
         
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-            }
-        }
+        if(memo[i][j] != -1)
+            return memo[i][j];
         
-        return dp[m-1][n-1];
+        visited[i][j] = true;
+        
+        memo[i][j] = solveMem(m, n, i+1, j, visited, memo) + solveMem(m, n, i, j+1, visited, memo);
+        
+        visited[i][j] = false;
+        
+        return memo[i][j];
+    }
+    
+    private int solveRec(int m, int n, int i, int j, boolean[][]visited){
+        if(i<0 || j<0 || i>=m || j>=n || visited[i][j] == true)
+            return 0;
+        
+        if(i == m-1 && j==n-1)
+            return 1;
+        
+        visited[i][j] = true;
+        
+        int res = solveRec(m, n, i+1, j, visited) + solveRec(m, n, i, j+1, visited);
+        
+        visited[i][j] = false;
+        
+        return res;
     }
 }
