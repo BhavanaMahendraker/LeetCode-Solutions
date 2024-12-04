6116c2fc8b359737d5d1dd76b0e95150a584a731
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-03 23:35:12 -0800

    Time: 1 ms (8.82%), Space: 40.3 MB (53.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,37 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         // return solveRec(m, n, 0, 0, new boolean[m][n]);
-        int[][] memo = new int[m][n];
-        for(int[] row: memo){
+        
+//         int[][] memo = new int[m][n];
+//         for(int[] row: memo){
+//             Arrays.fill(row, -1);
+//         }
+//         return solveMem(m, n, 0, 0, new boolean[m][n], memo);
+        
+        return solveTab(m, n);
+    }
+    
+    private int solveTab(int m, int n){
+        int[][] dp = new int[m][n];
+        
+        for(int[] row: dp){
             Arrays.fill(row, -1);
         }
-        return solveMem(m, n, 0, 0, new boolean[m][n], memo);
+        
+        for(int i=0; i<n; i++){
+            dp[m-1][i] = 1;
+        }
+        for(int i=0; i<m; i++){
+            dp[i][n-1] = 1;
+        }
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                if(dp[i+1][j] != -1 && dp[i][j+1] != -1)
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        
+        return dp[0][0];
     }
     
     private int solveMem(int m, int n, int i, int j, boolean[][]visited, int[][] memo){
@@ -27,13 +53,14 @@ class Solution {
         return memo[i][j];
     }
     
+    // Time Limit Exceeded    
     private int solveRec(int m, int n, int i, int j, boolean[][]visited){
-        if(i<0 || j<0 || i>=m || j>=n || visited[i][j] == true)
+        if(i<0 || j<0 || i>=m || j>=n || visited[i][j] == true){
             return 0;
-        
-        if(i == m-1 && j==n-1)
+        }
+        if(i == m-1 && j==n-1){
             return 1;
-        
+        }
         visited[i][j] = true;
         
         int res = solveRec(m, n, i+1, j, visited) + solveRec(m, n, i, j+1, visited);
