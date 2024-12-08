9917812d5b883d16fe1596d434f6680fcaa31c1b
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-07 22:28:55 -0800

    Time: 56 ms (44.12%), Space: 45.2 MB (93.76%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,22 +1,60 @@
 class Solution {
+    
+    // NOTE: Answer from 1143. Longest Common Subsequence
+    
+    
+    
     public int longestPalindromeSubseq(String s) {
-        int[][]dp = new int[s.length()][s.length()];
+        // return solveRec(text1, text2, 0, 0);
         
-        for(int i=0; i<s.length(); i++){
-            dp[i][i] = 1;
-        }
+        // int[][]memo = new int[text1.length()][text2.length()];
+        // for(int[] row: memo){
+        //     Arrays.fill(row, -1);
+        // }
+        // return solveMemo(text1, text2, 0, 0, memo);
         
-        for(int l=1; l < dp.length; l++){
-            for(int i= 0; i < dp.length - l; i++){
-                int j = i + l;
-                if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = 2 + dp[i+1][j-1];
-                } else{
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
-                }
-            }
-        }
+        // return solveTab(text1, text2);
+        // return solveTabSpaceOptimized(text1, text2);
         
-        return dp[0][dp.length-1];
+        String reverseS = new StringBuilder(s).reverse().toString();
+        
+        return solveTabFurtherOptimized(s, reverseS);
     }
+    
+    // Further Space Optimized (Look At This Once!)
+    // TC: O(text1Length * text2Length) This is my guess
+    // SC: O( min(text1Length, text2Length) ) This is my guess
+    public int solveTabFurtherOptimized(String text1, String text2){
+        
+        // NOTE: minimize space by swapping text2 with text1
+        if(text2.length() > text1.length()){
+            String temp = text1;
+            text1 = text2;
+            text2 = temp;
+        }
+        
+        int[]next = new int[text2.length() + 1];
+                
+        for(int i=text1.length()-1; i>=0; i--){
+            // Define curr array inside loop so no need to re-initialize
+            int[]curr = new int[text2.length() + 1];
+            
+            for(int j=text2.length()-1; j>=0; j--){
+                int ans = 0;
+                if(text1.charAt(i) == text2.charAt(j)){
+                    ans = 1 + next[j+1];
+                } else{
+                    ans = Math.max(next[j], curr[j+1]);
+                }
+                curr[j] = ans;
+            }
+            
+            next = curr;
+            // No need to re-initialize
+            // curr = new int[text2.length() + 1];
+        }
+        
+        return next[0];
+    }
+    
 }
