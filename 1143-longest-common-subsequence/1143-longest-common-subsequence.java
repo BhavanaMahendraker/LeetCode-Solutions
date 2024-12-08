34477e6801c47b51cad18d80a80a545cebc9b0ea
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-07 22:05:09 -0800

    Time: 17 ms (93.09%), Space: 45.3 MB (91.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -8,11 +8,37 @@ class Solution {
         // }
         // return solveMemo(text1, text2, 0, 0, memo);
         
-        return solveTab(text1, text2);
+        // return solveTab(text1, text2);
+        return solveTabSpaceOptimized(text1, text2);
     }
     
-    // TC: O(text1Length + text2Length) This is my guess
-    // SC: O(text1Length + text2Length) This is my guess
+    // Space Optimized
+    // TC: O(text1Length * text2Length) This is my guess
+    // SC: O(text2Length) This is my guess
+    public int solveTabSpaceOptimized(String text1, String text2){
+        int[]curr = new int[text2.length() + 1];
+        int[]next = new int[text2.length() + 1];
+                
+        for(int i=text1.length()-1; i>=0; i--){
+            for(int j=text2.length()-1; j>=0; j--){
+                int ans = 0;
+                if(text1.charAt(i) == text2.charAt(j)){
+                    ans = 1 + next[j+1];
+                } else{
+                    ans = Math.max(next[j], curr[j+1]);
+                }
+                curr[j] = ans;
+            }
+            
+            next = curr;
+            curr = new int[text2.length() + 1];
+        }
+        
+        return next[0];
+    }
+    
+    // TC: O(text1Length * text2Length) This is my guess
+    // SC: O(text1Length * text2Length) This is my guess
     public int solveTab(String text1, String text2){
         int[][]dp = new int[text1.length() + 1][text2.length() + 1];
                 
@@ -29,8 +55,8 @@ class Solution {
         return dp[0][0];
     }
     
-    // TC: O(text1Length + text2Length) This is my guess
-    // SC: O(text1Length + text2Length) This is my guess
+    // TC: O(text1Length * text2Length) This is my guess
+    // SC: O(text1Length * text2Length) This is my guess
     public int solveMemo(String text1, String text2, int i, int j, int[][] memo){
         if(i >= text1.length() || j >= text2.length())
             return 0;
