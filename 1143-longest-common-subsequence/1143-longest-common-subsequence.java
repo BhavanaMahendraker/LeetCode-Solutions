99b71f790df83f8ca5b6cf8a940faad8ea4e946a
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-07 21:32:53 -0800

    Time: 29 ms (19.08%), Space: 51.3 MB (13.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,18 +1,40 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        // return solveRec(text1, text2, 0, 0);
         
-        for(int i=1; i<=text1.length(); i++){
-            for(int j=1; j<=text2.length(); j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } 
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        int[][]memo = new int[text1.length()][text2.length()];
+        for(int[] row: memo){
+            Arrays.fill(row, -1);
+        }
+        return solveMemo(text1, text2, 0, 0, memo);
+    }
+    
+    public int solveMemo(String text1, String text2, int i, int j, int[][] memo){
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+        
+        if(memo[i][j] != -1)
+            return memo[i][j];
+        
+        if(text1.charAt(i) == text2.charAt(j)){
+            memo[i][j] = 1 + solveMemo(text1, text2, i+1, j+1, memo);
+            return memo[i][j];
         }
         
-        return dp[dp.length-1][dp[0].length-1];
+        memo[i][j] = Math.max(solveMemo(text1, text2, i+1, j, memo), solveMemo(text1, text2, i, j+1, memo));
+        return memo[i][j];
+    }
+    
+    // TC: O(3^(text1Length + text2Length)) This is my guess
+    // SC: O(max(text1Length, text2Length)) This is my guess
+    // Time Limit Exceeded
+    public int solveRec(String text1, String text2, int i, int j){
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+        
+        if(text1.charAt(i) == text2.charAt(j))
+            return 1 + solveRec(text1, text2, i+1, j+1);
+        
+        return Math.max(solveRec(text1, text2, i+1, j), solveRec(text1, text2, i, j+1));
     }
 }
