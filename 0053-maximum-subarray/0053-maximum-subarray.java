0e68b0a20206ca3a0a1c036d2d3936c764f273d6
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-08 22:38:55 -0800

    Time: 1 ms (99.38%), Space: 57.1 MB (46.80%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,54 +1,32 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = nums[0];
         
-        // Kadane's Algorithm
-        // TC: O(n), SC: O(1)
-        int left=0, right=0, maxSum = nums[0], currSum = 0;
-        
-        while(right < nums.length){
+        for(int i=1; i<nums.length; i++){
             if(currSum < 0){
-                left = right;
                 currSum = 0;
             }
             
-            currSum += nums[right];
+            currSum += nums[i];
             maxSum = Math.max(maxSum, currSum);
-            right++;
         }
         
         return maxSum;
-       
-        // Brute Force Approach
-        // TC: O(n^2), SC: O(1)
-//         int maxSum = nums[0];
-        
-//         for(int i =0; i<nums.length; i++){
-//             int currSum = 0;
-//             for(int j=i; j<nums.length; j++){
-//                 currSum += nums[j];
-//                 maxSum = Math.max(maxSum, currSum);
-//             }
-//         }
-//         return maxSum;
     }
 }
 
-
 /*
 [-2,1,-3,4,-1,2,1,-5,4]
-         i
-                     j
-  
-  currSum = 4 + 1 = 5
-  maxSum = 6
-  
-  if currSum <0
-    currSum =0
+       i
     
-  currSum += nums[j]
-  
-  maxSum = max of maxSum, currSum
+currSum = -2
+maxSum = 1
 
+if currSum < nums[i]
+    currSum = 0
 
+currSum += nums[i]
+maxSum = max(maxSum, currSum)
 
 */
