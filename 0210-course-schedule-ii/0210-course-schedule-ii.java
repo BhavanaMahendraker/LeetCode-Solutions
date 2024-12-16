fd83f5d6fe7b89c5c674e3416785e816eea9a0f4
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-15 23:04:07 -0800

    Time: 7 ms (39.03%), Space: 46.2 MB (17.00%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -3,47 +3,51 @@ class Solution {
         HashSet<Integer> visited = new HashSet<>();
         HashSet<Integer> dfsVisited = new HashSet<>();
         HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<>();
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Integer> topoSort = new ArrayList<>();
         
+        // AdjList
         for(int[] pre: prerequisites){
             adjList.putIfAbsent(pre[0], new ArrayList<>());
-            adjList.putIfAbsent(pre[0], new ArrayList<>());
-            
             adjList.get(pre[0]).add(pre[1]);
         }
         
+        // Topological Sort
         for(int i=0; i<numCourses; i++){
-            if(!visited.contains(i) && isCycle(visited, dfsVisited, adjList, i, res)){
-                return new int[0];
+            if(!visited.contains(i)){
+                if(isCycle(visited, dfsVisited, adjList, i, topoSort)){
+                    return new int[0];
+                }
             }
         }
+
+        int[] res = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            res[i] = topoSort.get(i);
+        }
         
-        return res.stream().mapToInt(i -> i).toArray();
+        return res;
     }
     
     private boolean isCycle(HashSet<Integer> visited, 
                          HashSet<Integer> dfsVisited,
                          HashMap<Integer, ArrayList<Integer>> adjList, 
-                         int curr, ArrayList<Integer> res){
+                         int curr, ArrayList<Integer> topoSort){
         
         dfsVisited.add(curr);
+        visited.add(curr);
         
         for(int neigh: adjList.getOrDefault(curr, new ArrayList<>())){
-            if(dfsVisited.contains(neigh)){
-                return true;
-            }
-            else if(visited.contains(neigh)){
-                continue;
-            }
-            if(isCycle(visited, dfsVisited, adjList, neigh, res)){
+            if(!visited.contains(neigh)){
+                if(isCycle(visited, dfsVisited, adjList, neigh, topoSort)){
+                    return true;
+                }
+            } else if(/*visited.contains(neigh) &&*/ dfsVisited.contains(neigh)){
                 return true;
             }
         }
         
         dfsVisited.remove(curr);
-        
-        res.add(curr);
-        visited.add(curr);
+        topoSort.add(curr);
         
         return false;
     }
