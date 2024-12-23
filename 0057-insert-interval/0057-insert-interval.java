22865f6305d3229583793a39b555db477ed214b7
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-23 00:02:28 -0800

    Time: 2 ms (41.29%), Space: 44.9 MB (42.06%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,21 +1,14 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> res = new ArrayList<>();
-        boolean inserted = false;      
         
         for(int[] currInterval: intervals){
-            
-            if(inserted == true){
-                res.add(currInterval);
-                continue;
-            }
-            
-            if(currInterval[1] < newInterval[0]){
+            if(newInterval == null || currInterval[1] < newInterval[0]){
                 res.add(currInterval);
             } else if(newInterval[1] < currInterval[0]){
                 res.add(newInterval);
                 res.add(currInterval);
-                inserted = true;
+                newInterval = null;
             } else{
                 int updatedStart = Math.min(newInterval[0], currInterval[0]);
                 int updatedEnd = Math.max(newInterval[1], currInterval[1]);
@@ -23,7 +16,8 @@ class Solution {
                 newInterval = new int[]{updatedStart, updatedEnd};
             }
         }
-        if(inserted == false){
+        
+        if(newInterval != null){
             res.add(newInterval);
         }
         
