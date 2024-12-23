ae72dcab231cd4c9a80e3f903187cb61a13bbee7
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-23 00:35:04 -0800

    Time: 49 ms (44.42%), Space: 74.6 MB (14.95%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -9,15 +9,20 @@ class Solution {
         });
         
         int totalDeletions = 0;
-        int[] prev = intervals[0];
+        // int[] prev = intervals[0];
+        int prevEnd = intervals[0][1];
         
         for(int i=1; i<intervals.length; i++){
             int[] curr = intervals[i];
-            if(prev[1] <= curr[0]){
-                prev = curr;
+            if(prevEnd <= curr[0]){
+                // prev = curr;
+                prevEnd = curr[1];
             } else {
-                if(curr[1] < prev[1])
-                    prev = curr;
+                // if(curr[1] < prev[1])
+                //     prev = curr;
+                
+                prevEnd = Math.min(prevEnd, curr[1]);
+                
                 totalDeletions++;
             }
         }
