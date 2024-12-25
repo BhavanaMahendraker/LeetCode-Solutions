532e5e238d47d9dc38a05cf6320ee6a40ce6c28e
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-25 03:12:36 -0800

    Time: 1 ms (92.82%), Space: 42.1 MB (24.12%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,23 +1,69 @@
+class TrieNode{
+    TrieNode[] children;
+    boolean isEnd;
+    
+    public TrieNode(){
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+    
+    public void addWord(String word){
+        TrieNode curr = this;
+        
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch - 'a'] == null){
+                curr.children[ch-'a'] = new TrieNode();
+            }
+            curr = curr.children[ch-'a'];
+        }
+        
+        curr.isEnd = true;
+    }
+    
+    public boolean searchWord(String word){
+        TrieNode curr = this;
+        
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch - 'a'] == null){
+                return false;
+            }
+            curr = curr.children[ch - 'a'];
+        }
+        
+        return curr.isEnd;
+    }
+}
+
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         ArrayList<String> res = new ArrayList<String>();
+        TrieNode root = new TrieNode();
         
-        dfs(0, s, res, new ArrayList<>(), wordDict);
+        for(String word: wordDict){
+            root.addWord(word);
+        }
+        
+        dfs(0, s, res, new ArrayList<>(), wordDict, root);
         return res;
     }
     
-    private void dfs(int start, String s, ArrayList<String> res, ArrayList<String> comb, List<String> wordDict){
+    private void dfs(int start, String s, ArrayList<String> res, ArrayList<String> comb, List<String> wordDict, TrieNode root){
         if(start == s.length()){
             res.add(new String(String.join(" ", comb)));
         }
         
         for(int end = start; end < s.length(); end++){
-            if(!wordDict.contains(s.substring(start, end+1)) ){
+            
+            String substring = s.substring(start, end+1);
+            // if(!wordDict.contains(substring)){
+            //     continue;
+            // }
+            if(!root.searchWord(substring)){
                 continue;
             }
             
-            comb.add(s.substring(start, end+1));
-            dfs(end+1, s, res, comb, wordDict);
+            comb.add(substring);
+            dfs(end+1, s, res, comb, wordDict, root);
             comb.remove(comb.size()-1);
         }
     }
