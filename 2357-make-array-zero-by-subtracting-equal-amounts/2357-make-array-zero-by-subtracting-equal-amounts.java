221e35289dcc25436cf9a41464518f745f9d1dbc
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-25 00:19:32 -0800

    Time: 1 ms (94.72%), Space: 41.3 MB (59.04%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,42 +1,51 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        if(nums.length == 1){
-            if(nums[0] == 0)
-                return 0;
-            else 
-                return 1;
-        }
         
-        nonZeroPresent(nums);
-        int numOfOperations = 0;
-        
-        while(nonZeroPresent(nums)){
-            int min = findSmallestNonZero(nums);
-            for(int i=0; i<nums.length; i++){
-                if(nums[i] != 0){
-                    nums[i] -= min;
-                }
-            }
-            numOfOperations++;
+        // Simple answer = return the number of different values that are > 0
+        HashSet<Integer> uniqueNumSet = new HashSet<>();
+        for(int num: nums){
+            if(num > 0)
+                uniqueNumSet.add(num);
         }
-        return numOfOperations;
+        return uniqueNumSet.size();
+        
+//         if(nums.length == 1){
+//             if(nums[0] == 0)
+//                 return 0;
+//             else 
+//                 return 1;
+//         }
+        
+//         nonZeroPresent(nums);
+//         int numOfOperations = 0;
+        
+//         while(nonZeroPresent(nums)){
+//             int min = findSmallestNonZero(nums);
+//             for(int i=0; i<nums.length; i++){
+//                 if(nums[i] != 0){
+//                     nums[i] -= min;
+//                 }
+//             }
+//             numOfOperations++;
+//         }
+//         return numOfOperations;
     }
     
-    private boolean nonZeroPresent(int[] nums){
-        for(int num: nums){
-            if(num != 0)
-                return true;
-        }
-        return false;
-    }
+//     private boolean nonZeroPresent(int[] nums){
+//         for(int num: nums){
+//             if(num != 0)
+//                 return true;
+//         }
+//         return false;
+//     }
     
-    private int findSmallestNonZero(int []nums){
-        int min = Integer.MAX_VALUE;
-        for(int num: nums){
-            if(num != 0 && min > num){
-                min = num;
-            }
-        }
-        return min;
-    }
+//     private int findSmallestNonZero(int []nums){
+//         int min = Integer.MAX_VALUE;
+//         for(int num: nums){
+//             if(num != 0 && min > num){
+//                 min = num;
+//             }
+//         }
+//         return min;
+//     }
 }
