baf7ad0f7072c31720c56a8dcd0abf95da21ca07
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2024-12-26 18:15:50 -0800

    Time: 2 ms (71.58%), Space: 42.1 MB (83.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,69 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        int rows = grid.length, cols = grid[0].length, freshOranges = 0, minTime = 0;
         Queue<int[]> q = new LinkedList<>();
+        int timeTaken = 0;
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        int totalNumOfFreshOranges = 0;
         
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] == 2){
                     q.add(new int[]{i,j});
                 }
                 if(grid[i][j] == 1){
-                    freshOranges++;
+                    totalNumOfFreshOranges++;
                 }
             }
         }
         
-        if(freshOranges == 0)
+        if(totalNumOfFreshOranges == 0){
             return 0;
-        
+        }
         while(!q.isEmpty()){
             int size = q.size();
-            
+            timeTaken++;
             while(size-- > 0){
-                int[] coords = q.poll();
+                int curr[] = q.poll();
                 for(int[] dir: dirs){
-                    int x = coords[0] + dir[0];
-                    int y = coords[1] + dir[1];
+                    int i=curr[0]+dir[0];
+                    int j=curr[1]+dir[1];
                     
-                    if(x<0 || y<0 || x >= rows || y >= cols || grid[x][y] != 1)
+                    if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] != 1){
                         continue;
-                    
-                    grid[x][y] = 2;
-                    q.add(new int[]{x,y});
-                    freshOranges--;
+                    }
+                    // Mark as visited !!!!!!!!
+                    grid[i][j] = 0;
+                    totalNumOfFreshOranges--;
+                    q.add(new int[]{i,j});
                 }
             }
-            minTime++;
         }
-        
-        return freshOranges != 0 ? -1 : minTime -1;
+        return totalNumOfFreshOranges == 0 ? timeTaken-1 : -1;
     }
 }
+
+/*
+[2,1,1],
+[1,1,0],
+[0,1,1]
+
+
+
+
+
+dir ={{1,0}, {0,1}, {-1,0}, {0,-1}}
+while q not empty
+    time++
+    size = q.size
+    while size > 0
+        int []curr = q.poll
+        for(dir : dirs)
+            i = curr[0] + dir[0]
+            j = curr[1] + dir[1]
+            if i<0 || j<0 || i>grid.length || j>grid[0].length || grid[i][j] != 1
+                continue
+            q.add(grid[i][j])
+            
+    return time
+
+*/
