4179559ab7812189a7f22b46a3183dfb0abb1b52
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2025-12-05 00:35:21 -0800

    Time: 8 ms (57.30%) | Memory: 65.3 MB (27.63%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+
+        dfs(res, new ArrayList<>(), s, 0);
+        return res;
+    }
+
+    private void dfs(List<List<String>> res, List<String> partitions, String s, int start) {
+        if (start >= s.length()) {
+            if(start == s.length()){
+                res.add(new ArrayList<>(partitions));
+            }
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                partitions.add(s.substring(start, end + 1));
+                dfs(res, partitions, s, end + 1);
+                partitions.remove(partitions.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
