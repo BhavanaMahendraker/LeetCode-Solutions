c9a101ec317ba5f5e4e60eb8d4e5bea7bdc0d09a
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-04-19 21:03:14 -0700

    Time: 100 ms (89.60%) | Memory: 142.1 MB (38.57%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,27 +1,25 @@
 class NumMatrix {
+
     int[][] prefixMatrix;
     public NumMatrix(int[][] matrix) {
-        prefixMatrix = new int[matrix.length+1][matrix[0].length+1];
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        prefixMatrix = new int[matrix.length + 1][matrix[0].length + 1];
 
-        for(int i=1; i<prefixMatrix.length; i++){
-            for(int j=1; j<prefixMatrix[0].length; j++){
-                prefixMatrix[i][j] = prefixMatrix[i-1][j] + prefixMatrix[i][j-1] 
-                                        - prefixMatrix[i-1][j-1] + matrix[i-1][j-1];
+        for(int i=1; i<=ROWS; i++){
+            for(int j=1; j<=COLS; j++){
+                prefixMatrix[i][j] = matrix[i-1][j-1] 
+                + prefixMatrix[i-1][j]
+                + prefixMatrix[i][j-1] 
+                - prefixMatrix[i-1][j-1];
             }
         }
-
-        // for(int i=1; i<prefixMatrix.length; i++){
-        //     int rowPrefix = 0;
-        //     for(int j=1; j<prefixMatrix[0].length; j++){
-        //         rowPrefix += matrix[i-1][j-1];
-        //         prefixMatrix[i][j] = prefixMatrix[i-1][j] + rowPrefix;
-        //     }
-        // }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        row1++; col1++; row2++; col2++;
-        return prefixMatrix[row2][col2] - prefixMatrix[row1-1][col2] - prefixMatrix[row2][col1-1] + prefixMatrix[row1-1][col1-1];
+        return prefixMatrix[row2+1][col2+1] 
+        - prefixMatrix[row1][col2+1] 
+        - prefixMatrix[row2+1][col1] 
+        + prefixMatrix[row1][col1];
     }
 }
 
