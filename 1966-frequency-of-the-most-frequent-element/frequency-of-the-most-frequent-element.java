1d9597cc95cc571cbd870de794a7856e34989cc8
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-04-25 21:51:49 -0700

    Time: 33 ms (92.56%) | Memory: 94.8 MB (46.53%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -6,10 +6,12 @@ class Solution {
 
         for(int right = 0; right < nums.length; right++){
             total += nums[right];
+
             while((long)nums[right] * (right - left + 1) > total + k){
                 total -= nums[left];
                 left++;
             }
+            
             maxFreq = Math.max(maxFreq, right - left + 1);
         }
 
