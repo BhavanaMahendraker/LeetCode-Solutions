c4eb1d16ac558e478ffc6263752e9968edeec634
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-05-09 19:32:51 -0700

    Time: 0 ms (100.00%) | Memory: 46.5 MB (52.43%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -0,0 +1,45 @@
+class Solution {
+    private boolean union(int[]parent, int[] rank, int x, int y){
+        x = findParent(parent, x);
+        y = findParent(parent, y);
+
+        if(x == y){
+            return false;
+        }
+
+        if(rank[x] >= rank[y]){
+            parent[y] = x;
+            rank[x] += rank[y];
+        } else{
+            parent[x] = y;
+            rank[y] += rank[x];
+        }
+        return true;
+    }
+
+    private int findParent(int[]parent, int node){
+        while(node != parent[node]){
+            node = parent[node];
+        }
+        return node;
+    }
+
+    public boolean validTree(int n, int[][] edges) {
+        int [] parent = new int[n];
+        int [] rank = new int[n];
+        int countJoins = 0;
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        for(int[] edge: edges){
+            if(!union(parent, rank, edge[0], edge[1])){
+                return false;
+            }
+            countJoins++;
+        }
+
+        return countJoins == n-1;
+    }
+}
