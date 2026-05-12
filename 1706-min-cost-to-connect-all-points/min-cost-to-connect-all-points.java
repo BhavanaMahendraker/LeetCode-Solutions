3102eefaffb8464ac012dfa8858880b6157831f0
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-05-11 19:58:38 -0700

    Time: 158 ms (53.13%) | Memory: 139.9 MB (16.90%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,20 +1,6 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        HashMap<Integer, ArrayList<int[]>> adjList = new HashMap<>();
         HashSet<Integer> visited = new HashSet<>();
-        for(int i=0; i<points.length; i++){
-            for(int j=i+1; j<points.length; j++){
-                int[] p1 = points[i];
-                int[] p2 = points[j];
-                adjList.putIfAbsent(i, new ArrayList<>());
-                adjList.putIfAbsent(j, new ArrayList<>());
-
-                int dist = Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
-
-                adjList.get(i).add(new int[]{j, dist});
-                adjList.get(j).add(new int[]{i, dist});
-            }
-        }
 
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
         minHeap.add(new int[]{0,0});
@@ -22,17 +8,27 @@ class Solution {
         
         while(!minHeap.isEmpty() && visited.size() != points.length){
             int[] curr = minHeap.poll();
-            int node = curr[0];
+            int nodeIndex = curr[0];
             int weight =  curr[1];
-            if(visited.contains(node)){
+            if(visited.contains(nodeIndex)){
                 continue;
             }
 
-            visited.add(node);
+            visited.add(nodeIndex);
             total += weight;
 
-            for(int[] neigh: adjList.getOrDefault(node, new ArrayList<>())){
-                minHeap.add(neigh);
+            for(int neigh=0; neigh<points.length; neigh++){
+                if(visited.contains(neigh)){
+                    continue;
+                }
+
+                int[] p1 = points[nodeIndex];
+                int[] p2 = points[neigh];
+
+                int dist = Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+
+                minHeap.add(new int[]{neigh, dist});
+                minHeap.add(new int[]{nodeIndex, dist});
             }
         }
 
