5ddbd24967c895eac2304a6cc5427e5f0fc7c9dc
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-05-11 20:03:09 -0700

    Time: 160 ms (53.01%) | Memory: 139.5 MB (16.95%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -8,13 +8,13 @@ class Solution {
         
         while(!minHeap.isEmpty() && visited.size() != points.length){
             int[] curr = minHeap.poll();
-            int nodeIndex = curr[0];
+            int currIndex = curr[0];
             int weight =  curr[1];
-            if(visited.contains(nodeIndex)){
+            if(visited.contains(currIndex)){
                 continue;
             }
 
-            visited.add(nodeIndex);
+            visited.add(currIndex);
             total += weight;
 
             for(int neigh=0; neigh<points.length; neigh++){
@@ -22,13 +22,11 @@ class Solution {
                     continue;
                 }
 
-                int[] p1 = points[nodeIndex];
-                int[] p2 = points[neigh];
-
-                int dist = Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+                int dist = Math.abs(points[currIndex][0] - points[neigh][0]) 
+                + Math.abs(points[currIndex][1] - points[neigh][1]);
 
                 minHeap.add(new int[]{neigh, dist});
-                minHeap.add(new int[]{nodeIndex, dist});
+                minHeap.add(new int[]{currIndex, dist});
             }
         }
 
