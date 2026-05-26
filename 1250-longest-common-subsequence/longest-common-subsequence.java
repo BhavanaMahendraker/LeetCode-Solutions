4fb0b45466d7346b509a03c26294d6ea58a915ad
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-05-25 19:23:32 -0700

    Time: 21 ms (53.32%) | Memory: 46.8 MB (91.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -7,8 +7,29 @@ class Solution {
 
         // return dfs(0,0,text1,text2, memo);
 
-        return tabSoln(text1, text2);
+        // return tabSoln(text1, text2);
+        return tabSolnOptimized(text1, text2);
     }
+
+    private int tabSolnOptimized(String text1, String text2){
+        int m = text1.length(), n = text2.length();
+        int[] prev = new int[n+1];
+        int[] curr = new int[n+1];
+
+        for(int i=1; i <= m; i++){
+            for(int j=1; j <= n; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    curr[j] = 1 + prev[j-1];
+                } else {
+                    curr[j] = Math.max(prev[j], Math.max(curr[j-1], prev[j-1]));
+                }
+            }
+            prev = curr.clone();
+        }
+
+        return curr[n];
+    }
+
 
     private int tabSoln(String text1, String text2){
         int m = text1.length(), n = text2.length();
