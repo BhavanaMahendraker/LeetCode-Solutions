926f72236a604b70b0a51a15e0acb74b013131cb
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2026-05-25 19:18:44 -0700

    Time: 24 ms (31.89%) | Memory: 53.4 MB (88.16%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,11 +1,29 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] memo = new int[text1.length()][text2.length()];
-        for(int[] mem : memo){
-            Arrays.fill(mem, -1);
+        // int[][] memo = new int[text1.length()][text2.length()];
+        // for(int[] mem : memo){
+        //     Arrays.fill(mem, -1);
+        // }
+
+        // return dfs(0,0,text1,text2, memo);
+
+        return tabSoln(text1, text2);
+    }
+
+    private int tabSoln(String text1, String text2){
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for(int i=1; i <= text1.length(); i++){
+            for(int j=1; j <= text2.length(); j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], Math.max(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
         }
 
-        return dfs(0,0,text1,text2, memo);
+        return dp[text1.length()][text2.length()];
     }
 
     private int dfs(int i, int j, String text1, String text2, int[][] memo){
